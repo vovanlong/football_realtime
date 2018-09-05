020b18244746ce6d41debe562a930d4e6002fafe
vovanlong <vovanlong1997@gmail.com>
2018-09-06 03:28:01 +0700

    config server and test server

---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+'use strict'
+module.exports = function(_){
+    return{
+        SetRouting: function(router){
+            router.get('/', this.indexPage);
+        },
+        indexPage: function(req,res){
+            return res.render('index',{test: 'sd is a test'});
+        }
+    }
+}
