113fecaca24795a945fb1c288f9143400f913b9c
vovanlong <vovanlong1997@gmail.com>
2018-09-06 01:23:55 +0700

    first commit

---
+++ container.js
@@ -0,0 +1,14 @@
+const dependable = require('dependable');
+const path = require('path');
+
+const container = dependable.container();
+
+const simpleDependecies = [
+    ['_', 'lodash']
+];
+
+simpleDependecies.forEach(function(val){
+    container.register(val[0], function(){
+        return require(val[1]);
+    })
+});
